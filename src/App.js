de48ec492f430286fe956ca515eb02abdc7fcb0a
Sfimadi94 <sydeffect1994@gmail.com>
2019-09-04 11:01:37 -0500

    syed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,42 @@ import BotsPage from "./containers/BotsPage";
 import "./App.css";
 
 class App extends Component {
+  
+  constructor(){
+    super()
+    this.state ={
+      robots: [],
+      displayRobots: [],
+      userRobots: []
+
+    }
+  
+  }
+
+  componentDidMount(){
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        robots: data,
+        displayRobots: data
+      })
+    })
+  }
+
+  addBots = (robots) => {
+    let userArray = this.state.userRobots
+    userArray.push(robots)
+    this.setState({
+      userRobots: userArray
+        })
+
+  }
+
   render() {
     return (
       <div className="App">
-        <BotsPage />
+        <BotsPage robots={this.state.displayRobots} addBots ={this.addBots} userRobots = {this.state.userRobots} displayRobots ={this.state.displayRobots}/>
       </div>
     );
   }
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,25 @@
 import React from "react";
+import BotCard from "../components/BotCard"
+import YourBotArmy from "../containers/YourBotArmy"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  addUserBots = (bot) => {
+    let userArray = this.state.userRobots
+    userArray.push(bot)
+    this.setState({
+      userRobots: userArray
+        })
+    
+
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy addBots = {this.props.addBots} userRobots = {this.props.userRobots}/>
+        {this.props.robots.map(bot => <BotCard bot= {bot} addBots = {this.props.addBots} />)}
       </div>
     );
   }

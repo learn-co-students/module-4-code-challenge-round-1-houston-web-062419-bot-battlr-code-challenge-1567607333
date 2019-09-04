be477fa5af95eca2a038f239d0bbc8820869cea7
Aaron Briscoe <abriscoe2008gmail.com>
2019-09-04 11:00:40 -0500

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,18 +2,26 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
+	addToArmy = (bot) => {
+		let arr= this.props.myBots
+		arr.push(bot)
+		console.log(bot) 
+		this.setState({
+			myBots: arr
+		})
+	}
+
+	render(){
+		return (
+		<div className="ui four column grid">
+				<div className="row">
+					{this.props.bots.map(bot => <BotCard bot={bot} addToArmy={this.addToArmy} key={bot.id}/>)}
+				
+				</div>
+		</div>
+		);
+  	}
 
 };
 
